085ba85b7e2945088ba57f79c6785160e7b6eb96
boris <boris@gratchev.com>
2024-11-05 11:28:30 +0100

    accept date and time as string

--- main.go
+++ main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type CreateInfo struct {
+	Date string
+	Time string
+}
 
 func createLink(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	fmt.Fprint(w, "here's your link")
+	var createInfo CreateInfo
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&createInfo)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		fmt.Fprintf(w, "input error")
+		return
+	}
+
+	fmt.Printf("here's your link date: %v, time: %v\n", createInfo.Date, createInfo.Time)
+	fmt.Fprintf(w, "here's your link date: %v, time: %v", createInfo.Date, createInfo.Time)
 }
 
 func main() {
