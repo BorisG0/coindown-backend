bd6a80e933d772ee9c0b5d018d3615031ab7faac
boris <boris@gratchev.com>
2024-11-29 18:10:05 -0800

    write to sqlite3 db

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+var db *sql.DB
+var err error
 
 type CreateInfo struct {
 	Date string
@@ -20,11 +26,19 @@ func createLink(w http.ResponseWriter, r *http.Request) {
 	var createInfo CreateInfo
 
 	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&createInfo)
+	err = decoder.Decode(&createInfo)
 	if err != nil {
-		fmt.Println("Error: ", err)
+		fmt.Println("Error at decode: ", err)
 		fmt.Fprintf(w, "input error")
 		return
+	}
+
+	datetime := createInfo.Date + " " + createInfo.Time
+
+	stmt := `INSERT INTO sessions (datetime) VALUES (?)`
+	_, err = db.Exec(stmt, datetime)
+	if err != nil {
+		log.Fatalf("Failed to insert data: %v", err)
 	}
 
 	fmt.Printf("here's your link date: %v, time: %v\n", createInfo.Date, createInfo.Time)
@@ -32,7 +46,14 @@ func createLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	db, err = sql.Open("sqlite3", "../coindown.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	fmt.Println("Server started")
 	http.HandleFunc("/create", createLink)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("main over")
 }
